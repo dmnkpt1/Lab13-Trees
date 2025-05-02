920d843f1094cceef81761c58a385c1268d1aa33
dmnkpt1 <petrus.pn@ucu.edu.ua>
2025-05-02 09:37:39 +0300

    add solution for task 2

--- Sort binary tree by levels/sort_binary_tree_by_levels.py
+++ Sort binary tree by levels/sort_binary_tree_by_levels.py
@@ -1,1 +1,17 @@
+def tree_by_levels(node):
+    if node is None:
+        return list()
+    result = []
 
+    l1 = [node]
+    while l1:
+        n = l1.pop(0)
+        if n is None:
+            continue
+        result.append(n.value)
+        if n.left:
+            l1.append(n.left)
+        if n.right:
+            l1.append(n.right)
+
+    return result
